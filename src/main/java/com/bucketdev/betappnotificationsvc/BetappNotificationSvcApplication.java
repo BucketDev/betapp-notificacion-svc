39bedc0c0853c106b9a5f75b45056633300cc05e
vanfigo <rodrigo.loyola@live.com>
2019-09-06 19:04:28 -0500

    first commit

---
+++ src/main/java/com/bucketdev/betappnotificationsvc/BetappNotificationSvcApplication.java
@@ -0,0 +1,13 @@
+package com.bucketdev.betappnotificationsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetappNotificationSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetappNotificationSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bucketdev/betappnotificationsvc/BetappNotificationSvcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bucketdev.betappnotificationsvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BetappNotificationSvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
